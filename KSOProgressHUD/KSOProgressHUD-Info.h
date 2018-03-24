c2cd6b27b5b194bc409915fb587d22f13780d819
William Towe <willbur1984@gmail.com>
2018-03-24 18:33:13 -0400

    add proper copyright notices

--- KSOProgressHUD/KSOProgressHUD-Info.h
+++ KSOProgressHUD/KSOProgressHUD-Info.h
@@ -3,6 +3,7 @@
 //  KSOProgressHUD
 //
 //  Created by William Towe on 3/24/18.
+//  Copyright © 2018 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 //
